62d898c511000e4d08d962665fe47b00679ec83e
davidlimacardoso <david.lima.cd@gmail.com>
2024-10-26 17:14:25 -0300

    Fix: Check all responses from http batch, change payload to factory

--- src/scenarios/fakestore/fakestore_product_to_cart.js
+++ src/scenarios/fakestore/fakestore_product_to_cart.js
@@ -1,10 +1,12 @@
 import http from 'k6/http';
 import { sleep, check, group } from 'k6';
 import Strategy from '../../strategies/strategy.js';
+import FactoryFakestore from '../../factory/fakestore.js';
 
 const PARAMS = Strategy.getParams();
 const strategy = PARAMS[Strategy.getStrategy()] // Return the strategy inside src/strategies/env/{ENV}.json
 const baseUrl = PARAMS.host.fakestore; // Return the base URL inside src/strategies/env/{ENV}.json
+const factoryFakestore = new FactoryFakestore();
 
 // Options for the load test
 export const options = {
@@ -13,8 +15,8 @@ export const options = {
       fakestore_product_to_cart: strategy.fakestore_product_to_cart
     },
     thresholds: {
-      http_req_failed: ["rate < 0.01"],
-      http_req_duration: ["p(99.9) < 800"],
+      http_req_failed: ["rate < 0.1"],
+      http_req_duration: ["p(99.9) < 1000"],
     }
 }
 
@@ -36,10 +38,13 @@ export default function () {
         ['GET', `${baseUrl}/icons/intro.svg`, headers , { tags: { ctype: 'images' } }],
         ['GET', `${baseUrl}/js/polyfill.min.js`, headers , { tags: { ctype: 'js' } }],
       ]);
-    
-      check(responses[0], {
-        'status is 200': (r) => r.status === 200,
-        'Homepage loaded': (r) => JSON.stringify(r)
+      
+      // Loop to see each response api or url
+      responses.forEach((response) => {
+        check(response, {
+            'status is 200': (r) => r.status === 200,
+            'Homepage loaded': (r) => r.body.length > 0,
+        });
       });
     
       sleep(4);
@@ -52,15 +57,17 @@ export default function () {
         ['GET', `${baseUrl}/products`, headers],
         ['GET', `${baseUrl}/products/categories`, headers],
         ['GET', `${baseUrl}/products/category/jewelery`, headers],
-        ['GET', `${baseUrl}/products/products/1`, headers],
+        ['GET', `${baseUrl}/products/1`, headers],
         ['GET', `${baseUrl}/carts?userId=1`, headers],
 
     ]);
 
-    check(responses[0], {
-        'status is 200': (r) => r.status === 200,
-        'Products loaded': (r) => JSON.stringify(r),
-    });
+      // Loop to see each response api or url
+      responses.forEach((response) => {
+        check(response, {
+            'status is 200': (r) => r.status === 200
+        });
+      });
 
     sleep(1);
   });
@@ -68,15 +75,10 @@ export default function () {
   // 03. Include product to cart
   group('Fakestore - Include Product to Cart', () => {
 
-    let data = {
-        "userId": 1,
-        "date": "2020-03-02",
-        "products": [{"productId": 1, "quantity": 3}]
-    }
-
-    let responses = http.put(`${baseUrl}/carts/1`, data, headers);
+    let data = factoryFakestore.getCartProductPayload()
+    let response = http.put(`${baseUrl}/carts/1`, data, headers);
     
-    check(responses, {
+    check(response, {
         'status is 200': (r) => r.status === 200,
         'Included to cart': (r) => r.body.includes('"id":1')
     });
